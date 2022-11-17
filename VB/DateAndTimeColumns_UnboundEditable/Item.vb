24bb4193c95e77fefd5ae11da2c418881f6660ea
DevExpressExampleBot <fakeEmail>
2022-11-17 18:34:16 +0400

    autogenerate VB

---
+++ VB/DateAndTimeColumns_UnboundEditable/Item.vb
@@ -0,0 +1,21 @@
+Imports DevExpress.Mvvm
+
+Namespace DateAndTimeColumns_UnboundEditable
+
+    Public Class Item
+        Inherits BindableBase
+
+        Private dateTimeField As Date
+
+        Public Property DateTime As Date
+            Get
+                Return dateTimeField
+            End Get
+
+            Set(ByVal value As Date)
+                dateTimeField = value
+                RaisePropertiesChanged()
+            End Set
+        End Property
+    End Class
+End Namespace
